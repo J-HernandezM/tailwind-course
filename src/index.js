c9e31a77103e6599e033c497eac4ac8d68abcf5f
Juan <juansw03@gmail.com>
2023-12-01 17:48:22 -0500

    feat: nav bar desktop

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { flights } from "./flights.js"
 import { faqs } from "./faq.js"
 
-const recommendContainer = document.querySelector('.recommended')
-const faqContainer = document.querySelector('.faq')
+const recommendContainer = document.querySelector('#recommended')
+const faqContainer = document.querySelector('#faq')
 const icons = [...document.querySelectorAll('.icons')]
 
 let scrollBox = document.createElement('div')
@@ -23,7 +23,7 @@ const toggleActive = (e) => {
 
 flights.forEach( flight => {
     const structure = `
-        <div class="shrink-0 w-40 h-48 rounded-lg text-white md:w-72 md:h-80 bg-secondary transform transition-all hover:-translate-y-4 hover:shadow-xl">
+        <div class="shadow-sm shrink-0 w-40 h-48 rounded-lg text-white md:w-72 md:h-80 bg-secondary transform transition-all hover:-translate-y-4 hover:shadow-xl">
             <img class="w-full rounded-t-lg h-3/5" src="${flight.image}" alt="${flight.title} flight"></img>
             <div class="h-2/5 pl-2.5 pt-3 md:pl-[18px] md:pt-5">
                 <h3 class="font-bold text-sm md:mb-2 md:text-2xl">${flight.title}</h3>
